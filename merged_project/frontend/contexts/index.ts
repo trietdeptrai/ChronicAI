7201089457853716dc70391fe6c54b2e74cff439
Phong Le <lehongphong270704@gmail.com>
2026-02-07 21:55:02 +0700

    Resolve conflicts

--- merged_project/frontend/contexts/index.ts
+++
@@ -1,3 +0,0 @@
-// Re-export all contexts
-export * from "./query-provider"
-export * from "./auth-context"
--- merged_project/frontend/lib/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-// Re-export all hooks
-export * from "./use-patients"
-export * from "./use-chat"
-export * from "./use-doctor-chat"
-export * from "./use-debounce"
